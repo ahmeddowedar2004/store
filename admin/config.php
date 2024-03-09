561f0609f4f6cb6c3949a3296e6d3b461061c32e
Ahmed Dowedar <149760353+ahmeddowedar2004@users.noreply.github.com>
2024-03-10 01:36:16 +0200

    Update config.php

--- admin/config.php
+++ admin/config.php
@@ -1,3 +1,3 @@
 <?php
-$con = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$con = mysqli_connect('sql110.infinityfree.com','if0_35866872','HAhmed2022','if0_35866872_shop_db') or die('connection failed');
 ?>
